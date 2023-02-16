38776f2fe31ae5aee0a442461ebe4391cc6da1c5
sunny <601619961@qq.com>
2023-02-16 17:08:00 +0800

    v0.5.5

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="xtreme1",
+    name="xtreme1-sdk",
     version="0.5.5",
     author="Basic AI",
     author_email="nico@basic.ai",
     description="xtreme1 sdk",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/xtreme1-io/xtreme1-SDK.git",
+    url="https://github.com/xtreme1-io/xtreme1-sdk.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
